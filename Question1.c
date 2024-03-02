4d158a642c5df658503e9e78de01836526033bab
Shivraj-Pun <118063371+Shivraj-Pun@users.noreply.github.com>
2024-03-02 17:29:16 +0530

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -1,1 +1,33 @@
-
+#include <stdio.h>
+int main() 
+{
+  int n;
+  printf("Enter number of elements in array: ");
+  scanf("%d", &n);
+  int a[n],b[n];
+  for(int i=0; i<n ; i++)
+    {
+      printf("Enter element of array:");
+      scanf("%d",&a[i]);
+    }
+  printf("\nInputed array is:");
+  for(int i=0; i<n ; i++)
+  {
+    printf("%d",a[i]);
+  }
+  for(int i=0; i<n/2 ; i++)
+  {
+    for(int j=n; j; j++)
+      {
+        int temp=a[i];
+        a[i]=a[n-i-1];
+        a[n-i-1]=temp;
+      } 
+  }
+  printf("\nReversed array is :");
+  for(int i=0; i<n; i++)
+    {
+      printf("%d",a[i]);
+    }
+  return 0;
+}
